5c061ed4ff0032072364c97e73abe7612bf94082
fuentesf <fuentesf@GMXMEXDELAPFJJI>
2019-11-19 17:35:37 -0600

    dolares dolares FFUENTES 20191119

--- Siniestros/OrdenPago.aspx.vb
+++ Siniestros/OrdenPago.aspx.vb
@@ -1759,11 +1759,27 @@ Partial Class Siniestros_OrdenPago
                     oImpuestos.AppendFormat("<Base>{0}</Base>", IIf(iMonedaPoliza = 0, (CDbl(oFila("Pago")) - CDbl(oFila("Descuentos"))), Math.Round(CDbl(oFila("Pago") - CDbl(oFila("Descuentos"))) / CDbl(Me.txtTipoCambio.Text), 2)))
                     oImpuestos.AppendFormat("<ImporteNoGravado>{0}</ImporteNoGravado>", IIf(iMonedaPoliza = 0, CDbl(oDetalle("ImporteNoGravado")), Math.Round(CDbl(oDetalle("ImporteNoGravado")) / CDbl(Me.txtTipoCambio.Text), 2)))
                     'oImpuestos.AppendFormat("<ImporteImpuesto>{0}</ImporteImpuesto>", IIf(iMonedaPoliza = 0, CDbl(oDetalle("ImporteImpuesto")), Math.Round(CDbl(oDetalle("ImporteImpuesto")) / CDbl(Me.txtTipoCambio.Text), 2)))
-                    oImpuestos.AppendFormat("<ImporteImpuesto>{0}</ImporteImpuesto>", IIf(iMonedaPoliza = 0, CDbl(oDatos.Tables(0).Rows(inum_impuesto).Item("ImporteImpuesto")), Math.Round(CDbl(oDatos.Tables(0).Rows(inum_impuesto).Item("ImporteImpuesto")) / CDbl(Me.txtTipoCambio.Text), 2)))
+                    If chkVariosConceptos.Checked = True Then
+                        If cmbMonedaPago.SelectedValue = 1 Then
+                            oImpuestos.AppendFormat("<ImporteImpuesto>{0}</ImporteImpuesto>", IIf(iMonedaPoliza = 0, CDbl(oDatos.Tables(0).Rows(0).Item("ImporteImpuesto")), Math.Round(CDbl(oDatos.Tables(0).Rows(0).Item("ImporteImpuesto")), 2)))
+                        Else
+                            oImpuestos.AppendFormat("<ImporteImpuesto>{0}</ImporteImpuesto>", IIf(iMonedaPoliza = 0, CDbl(oDatos.Tables(0).Rows(0).Item("ImporteImpuesto")), Math.Round(CDbl(oDatos.Tables(0).Rows(0).Item("ImporteImpuesto")) / CDbl(Me.txtTipoCambio.Text), 2)))
+                        End If
+                    Else
+                        If cmbMonedaPago.SelectedValue = 1 Then
+                            oImpuestos.AppendFormat("<ImporteImpuesto>{0}</ImporteImpuesto>", IIf(iMonedaPoliza = 0, CDbl(oDatos.Tables(0).Rows(inum_impuesto).Item("ImporteImpuesto")), Math.Round(CDbl(oDatos.Tables(0).Rows(inum_impuesto).Item("ImporteImpuesto")), 2)))
+                        Else
+                            oImpuestos.AppendFormat("<ImporteImpuesto>{0}</ImporteImpuesto>", IIf(iMonedaPoliza = 0, CDbl(oDatos.Tables(0).Rows(inum_impuesto).Item("ImporteImpuesto")), Math.Round(CDbl(oDatos.Tables(0).Rows(inum_impuesto).Item("ImporteImpuesto")) / CDbl(Me.txtTipoCambio.Text), 2)))
+                        End If
+                    End If
                     oImpuestos.AppendFormat("<PjeRetencion>{0}</PjeRetencion>", IIf(iMonedaPoliza = 0, CDbl(oDetalle("PjeRetencion")), Math.Round(CDbl(oDetalle("PjeRetencion")) / CDbl(Me.txtTipoCambio.Text), 2)))
                     'oImpuestos.AppendFormat("<ImporteRetencion>{0}</ImporteRetencion>", IIf(iMonedaPoliza = 0, CDbl(oDetalle("ImporteRetencion")), Math.Round(CDbl(oDetalle("ImporteRetencion")) / CDbl(Me.txtTipoCambio.Text), 2)))
-                    oImpuestos.AppendFormat("<ImporteRetencion>{0}</ImporteRetencion>", IIf(iMonedaPoliza = 0, CDbl(oDatos.Tables(0).Rows(inum_impuesto).Item("ImporteRetencion")), Math.Round(CDbl(oDatos.Tables(0).Rows(inum_impuesto).Item("ImporteRetencion")) / CDbl(Me.txtTipoCambio.Text), 2)))
 
+                    If chkVariosConceptos.Checked = True Then
+                        oImpuestos.AppendFormat("<ImporteRetencion>{0}</ImporteRetencion>", IIf(iMonedaPoliza = 0, CDbl(oDatos.Tables(0).Rows(0).Item("ImporteRetencion")), Math.Round(CDbl(oDatos.Tables(0).Rows(0).Item("ImporteRetencion")) / CDbl(Me.txtTipoCambio.Text), 2)))
+                    Else
+                        oImpuestos.AppendFormat("<ImporteRetencion>{0}</ImporteRetencion>", IIf(iMonedaPoliza = 0, CDbl(oDatos.Tables(0).Rows(inum_impuesto).Item("ImporteRetencion")), Math.Round(CDbl(oDatos.Tables(0).Rows(inum_impuesto).Item("ImporteRetencion")) / CDbl(Me.txtTipoCambio.Text), 2)))
+                    End If
                     oImpuestos.AppendFormat("<CodigoTratamiento>{0}</CodigoTratamiento>", CInt(oDetalle("CodigoTratamiento")))
                     oImpuestos.AppendFormat("<Subsiniestro>{0}</Subsiniestro>", CInt(oFila.Item("Subsiniestro")))
 
