1e594d1a902b594e666e90f3ca6ae7fce353436f
martinem <martinem@GMXMEXDELAPFMIQ>
2020-03-19 17:01:02 -0600

    Reporte de Conta Electronica

---
+++ Siniestros/ContaElectronica.aspx.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Data
+
+Partial Class Siniestros_ContaElectronica
+    Inherits System.Web.UI.Page
+
+    Private Sub btn_Reporte_Click(sender As Object, e As EventArgs) Handles btn_Reporte.Click
+        Dim ws As New ws_Generales.GeneralesClient
+        Dim server As String = ws.ObtieneParametro(9)
+        Try
+
+
+            If txtFecGeneraDe.Text <> vbNullString Then
+                If txtFecGeneraA.Text <> vbNullString Then
+
+                    Dim RptFilters As String
+                    RptFilters = "&FechaIni=" & CDate(txtFecGeneraDe.Text).ToString("yyyyMMdd")
+                    RptFilters = RptFilters & "&Fechafin=" & CDate(txtFecGeneraA.Text).ToString("yyyyMMdd")
+
+                    server = Replace(Replace(server, "@Reporte", "ContaElect"), "@Formato", "EXCEL")
+                    server = Replace(server, "ReportesGMX_UAT", "ReportesOPSiniestros")
+                    server = server & RptFilters
+                    Funciones.EjecutaFuncion("window.open('" & server & "');")
+                Else
+                    Mensaje.MuestraMensaje("Reporte Contabilidad Electronica", "Debe seleccionar la fecha final", Mensaje.TipoMsg.Advertencia)
+
+                End If
+            Else
+                Mensaje.MuestraMensaje("Reporte Contabilidad Electronica", "Debe seleccionar una fecha inicial", Mensaje.TipoMsg.Advertencia)
+            End If
+
+
+
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
